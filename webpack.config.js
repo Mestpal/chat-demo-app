9d0fe152ac6d03b3b00ad236c983d54c1a11b955
Mestpal <manuestpal@gmail.com>
2020-09-12 17:32:36 +0200

    Add css, html loaders and devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
   mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: './src/index.js',
   module: {
     rules: [
       {
@@ -14,11 +17,22 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
       }
     ]
   },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-};
+  }
+}
