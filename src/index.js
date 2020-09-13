031706913bda27e578da1790c471dad719eae1f5
Mestpal <manuestpal@gmail.com>
2020-09-13 21:32:18 +0200

    Maintain friendship status in localStorage

--- src/index.js
+++ src/index.js
@@ -5,25 +5,45 @@ import * as routing from './router'
 
 (function (){
   function init () {
+    document.addEventListener('click', clickAddFriend)
+
+    if (localStorage.getItem('friendStatus')) {
+      let target = null
+      let buttons = document.getElementsByName('addAsFriend')
+
+      for (const index in buttons) {
+        updateProfile(JSON.parse(localStorage.getItem('friendStatus')), buttons.item(index))
+      }
+    }
+
     var router = new routing.Router([
       new routing.Route('profile', true),
       new routing.Route('chat')
     ])
   }
   init()
-
-  document.addEventListener('click', clickAddFriend)
-  function clickAddFriend (event) {
-    let element = event.target
-    if (element.classList.contains('btn') && element.innerText === "ADD AS FRIEND") {
-        const profilePage = document.getElementsByClassName('page__profile')[0]
-        if (profilePage.classList.contains('page__profile--alt')) {
-          profilePage.setAttribute('class', 'page__profile')
-          element.setAttribute('class', 'btn')
-        } else {
-          profilePage.setAttribute('class', 'page__profile page__profile--alt')
-          element.setAttribute('class', 'btn btn--alt')
-        }
-      }
-  }
 }())
+
+function clickAddFriend (event) {
+  let target = event.target
+  if (target.classList.contains('btn') && target.innerText === "ADD AS FRIEND") {
+    if(localStorage.getItem('friendStatus') === 'true') {
+      localStorage.setItem('friendStatus', 'false')
+    } else {
+      localStorage.setItem('friendStatus', 'true')
+    }
+    updateProfile(JSON.parse(localStorage.getItem('friendStatus')), target)
+  }
+}
+
+function updateProfile (friendStatus, target = null) {
+  const profilePage = document.getElementsByClassName('page__profile')[0]
+
+  if (JSON.parse(friendStatus) ) {
+    profilePage.setAttribute('class', 'page__profile page__profile--alt')
+    target ? target.setAttribute('class', 'btn btn--alt') : ''
+  } else {
+    profilePage.setAttribute('class', 'page__profile')
+    target ? target.setAttribute('class', 'btn') : ''
+  }
+}
