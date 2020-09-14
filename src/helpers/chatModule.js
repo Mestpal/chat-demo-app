6c19bc6f5226226e0c78329e622d601228f5168b
Mestpal <manuestpal@gmail.com>
2020-09-14 12:41:32 +0200

    Logic to save/render chat messages

--- src/helpers/chatModule.js
+++ src/helpers/chatModule.js
@@ -2,23 +2,49 @@ let sendMessage = function () {
   const input = document.getElementsByTagName('input').item(0)
 
   if (input.value) {
-    const message = {id:Date.now(), user:'me', text: input.value}
+    const message = {id: Date.now(), user: 'me', text: input.value}
     input.value = ''
     updateChatHistory(message)
   }
+  renderChatConversation(JSON.parse(localStorage.getItem('conversation')))
 }
 
 function updateChatHistory (message) {
   let conversation = null
 
   if (!localStorage.getItem('conversation')) {
-    console.log('crear conversacion en ls')
     conversation = [message]
   } else {
     conversation = JSON.parse(localStorage.getItem('conversation'))
     conversation.push(message)
   }
+
   localStorage.setItem('conversation', JSON.stringify(conversation))
 }
 
-export {sendMessage}
+function renderChatConversation (conversation) {
+  try {
+    const messagesBox = document.getElementsByClassName('chat__messages').item(0)
+    conversation.map((message) => {
+      let isNotRender = true
+
+      for (const index in messagesBox.children) {
+        if (messagesBox.children[index].id === `${message.id}`) {
+          isNotRender = false
+        }
+      }
+
+      if (isNotRender) {
+        let entry = document.createElement('li')
+        entry.id = message.id
+        entry.textContent = message.text
+        entry.setAttribute('class', message.user)
+        messagesBox.appendChild(entry)
+      }
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export {sendMessage, renderChatConversation}
