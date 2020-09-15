1bf6e9d3da71376d842ba8536e9816be0a241e7a
Mestpal <manuestpal@gmail.com>
2020-09-15 20:51:38 +0200

    Test for Chat: instance and contains props

--- test/unit/chat/chat.test.js
+++ test/unit/chat/chat.test.js
@@ -1,3 +1,15 @@
-test('example',() => {
-  console.log('example')
+import Chat from '../../../src/chat/index'
+
+describe('chat', () => {
+  test('if is instance of Chat',() => {
+    let chat = new Chat([])
+    chat.init = jest.fn()
+    expect(chat instanceof Chat).toBeTruthy
+  })
+
+  test('if contains expect props', () => {
+    const expected = { conversation: [] }
+    let chat = new Chat([])
+    expect(chat).toMatchObject(expected)
+  })
 })
