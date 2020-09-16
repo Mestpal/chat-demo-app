96b802d104158d11d992b05b51ab0c71c874acb6
Manuel Estevez <manuestpal@gmail.com>
2020-09-16 18:31:01 +0200

    App in a new file

---
+++ src/core/App.js
@@ -0,0 +1,32 @@
+import * as routing from './router'
+import * as helpers from '../helpers'
+import Chat from './chat'
+
+export default function App() {
+    this.init()
+  }
+
+  App.prototype = {
+    init () {
+      helpers.listenersModule.listeners()
+
+      var chat = new Chat(JSON.parse(localStorage.getItem('conversation')))
+
+      window.addEventListener('hashchange', (e) => {
+        const timer = setTimeout(() => {
+          if(window.location.hash.substr(1) === 'chat') {
+            const randomMessage = chat.createMessage(Math.random().toString(36).substr(2, 5), 'friend')
+            chat.updateChatHistory(randomMessage)
+            chat.renderChatConversation(JSON.parse(localStorage.getItem('conversation')))
+            chat.scrollToLast('chatMessage')
+          }
+          clearTimeout(timer)
+        }, 500)
+      })
+
+      var router = new routing.Router([
+        new routing.Route('profile', true),
+        new routing.Route('chat')
+      ])
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -4,37 +4,6 @@ import profileImg from '../assests/images/husky-girl.jpg'
 import profileImgSmall from '../assests/images/husky-girl-small.jpg'
 import profileUserImgSmall from '../assests/images/husky-small.jpg'
 
-import * as routing from './core/router'
-import * as helpers from './helpers'
-import Chat from './core/chat'
-
-function App() {
-  this.init()
-}
-
-App.prototype = {
-  init () {
-    helpers.listenersModule.listeners()
-
-    var chat = new Chat(JSON.parse(localStorage.getItem('conversation')))
-
-    window.addEventListener('hashchange', (e) => {
-      const timer = setTimeout(() => {
-        if(window.location.hash.substr(1) === 'chat') {
-          const randomMessage = chat.createMessage(Math.random().toString(36).substr(2, 5), 'friend')
-          chat.updateChatHistory(randomMessage)
-          chat.renderChatConversation(JSON.parse(localStorage.getItem('conversation')))
-          chat.scrollToLast('chatMessage')
-        }
-        clearTimeout(timer)
-      }, 500)
-    })
-
-    var router = new routing.Router([
-      new routing.Route('profile', true),
-      new routing.Route('chat')
-    ])
-  }
-}
+import App from './core/App'
 
 new App()
