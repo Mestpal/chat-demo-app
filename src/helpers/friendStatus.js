9a0f647a5dcecfe0dd718c08d5413a742adbbedb
Manuel Estevez <manuestpal@gmail.com>
2020-09-17 11:35:35 +0200

    Refactor updateProfile

--- src/helpers/friendStatus.js
+++ src/helpers/friendStatus.js
@@ -1,16 +1,27 @@
 export default function listeners() {
-  (() => {
-    document.getElementsByName('addAsFriend').forEach((element) => {
-      element.onclick = clickAddFriend
+  document.getElementsByName('addAsFriend').forEach((element) => {
+    element.addEventListener('click', (e) => {
+      clickAddFriend(e)
+      updateProfile(JSON.parse(localStorage.getItem('friendStatus')),
+        {
+          profilePage: document.getElementsByClassName('page__profile').item(0),
+          friendButton: e.target
+        }
+      )
     })
+  })
 
-    if (localStorage.getItem('friendStatus')) {
-      let buttons = document.getElementsByName('addAsFriend')
-      for (const index in buttons) {
-        updateProfile(JSON.parse(localStorage.getItem('friendStatus')), buttons.item(index))
-      }
+  if (localStorage.getItem('friendStatus')) {
+    let buttons = document.getElementsByName('addAsFriend')
+    for (const index in buttons) {
+      updateProfile(JSON.parse(localStorage.getItem('friendStatus')),
+        {
+          profilePage: document.getElementsByClassName('page__profile').item(0),
+          friendButton: buttons.item(index)
+        }
+      )
     }
-  })()
+  }
 }
 
 let clickAddFriend = function (event) {
@@ -19,18 +30,16 @@ let clickAddFriend = function (event) {
   } else {
     localStorage.setItem('friendStatus', 'true')
   }
-  updateProfile(JSON.parse(localStorage.getItem('friendStatus')), event.target)
 }
 
-let updateProfile = function (friendStatus, target = null) {
-  const profilePage = document.getElementsByClassName('page__profile').item(0)
-
-  if (JSON.parse(friendStatus) ) {
-    profilePage.setAttribute('class', 'page__profile page__profile--alt')
-    target ? target.setAttribute('class', 'btn btn--alt') : ''
+let updateProfile = function (friendStatus, target = {}) {
+  if (friendStatus) {
+    console.log('pepe')
+    target.profilePage.setAttribute('class', 'page__profile page__profile--alt')
+    target.friendButton ? target.friendButton.setAttribute('class', 'btn btn--alt') : ''
   } else {
-    profilePage.setAttribute('class', 'page__profile')
-    target ? target.setAttribute('class', 'btn') : ''
+    target.profilePage.setAttribute('class', 'page__profile')
+    target.friendButton ? target.friendButton.setAttribute('class', 'btn') : ''
   }
 }
 
